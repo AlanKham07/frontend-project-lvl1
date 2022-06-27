d5054fa4bbc176ea9c5ebb2066a3172565b3d0dc
Alan Khamokov <alkham007@mail.ru>
2022-06-27 15:57:16 +0300

    шаг третий

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 install:
-	npmci
+	npm ci
 
 brain-games:
 	node bin/brain-games.js
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
+import {greetings} from '/home/alan/frontend-project-lvl1/src/cli.js' 
 console.log('Welcome to the Brain Games!');
-
+greetings();
