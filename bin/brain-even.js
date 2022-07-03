bfaca71251974aae34f31d6b16180932110a2bd0
Alan Khamokov <alkham007@mail.ru>
2022-07-03 15:41:53 +0300

    шаг 5

---
+++ bin/brain-even.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+  const game = () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May I have tour name? ');
+    const gameOver = '\'yes\' is wrong answer ;(. Correct answer was \'no\'.\nLet\'s try again, ' + userName + '!';
+    const win = `Congratulations, ${userName}!`;
+    console.log(`Hello, ${userName}!`);
+    console.log('Answer \"yes\" if the number is even, otherwise answer \"no\".');
+    for(let i = 0; i < 3; i++) {
+    const random = Math.round(Math.random() * 100);
+    const zadanie = 'Question: '
+    console.log(zadanie + random);
+    const otvet = readlineSync.question('Your answer: ');
+        if((random % 2 === 0 && otvet === 'yes') || (random % 2 !== 0 && otvet === 'no')) {
+        console.log('Correct!');
+    } else if((random % 2 === 0 && otvet !== 'yes') || (random % 2 !== 0 && otvet !== 'no')) {
+        console.log(gameOver);
+        return;
+    }
+
+}
+console.log(win);
+};
+
+game();
+
