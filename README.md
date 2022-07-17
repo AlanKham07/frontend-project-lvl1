b78c5b9970d5ea7672b868177125b5e3766fcf77
Alan Khamokov <alkham007@mail.ru>
2022-07-17 12:57:26 +0300

    шаг 7

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [![Actions Status](https://github.com/AlanKham07/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/AlanKham07/frontend-project-lvl1/actions)<a href="https://codeclimate.com/github/codeclimate/codeclimate/maintainability"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability" /></a>
 https://asciinema.org/a/wyLGztyB2hkjxGMKpKs6IZjhS
 https://asciinema.org/a/AnEUyCt57K44z3q8Y3clfbcCK
+https://asciinema.org/a/5ojdfcAD2n7gKLya2qmsduUsd
---
+++ games/brain-gcd.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const gcdGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have tour name? ');
+  console.log(`Hello, ${userName}!`);
+  const win = `Congratulations, ${userName}!`;
+  console.log('Find the greatest common divisor of given numbers.');
+  for (let i = 0; i < 3; i++) {
+    const zadanie = 'Question: ';
+    const numberOne = Math.round(Math.random() * 10);
+    const numberTwo = Math.round(Math.random() * 10);
+    const primer = `${numberOne} ${numberTwo}`;
+    console.log(zadanie + primer);
+    const otvet = readlineSync.question('Your answer: ');
+    const gcd = (a, b) => {
+      if (b === 0) {
+        return a;
+      }
+      return gcd(b, a % b);
+    };
+    const gcdOtvet = gcd(numberOne, numberTwo);
+    if (Number(otvet) === gcdOtvet) {
+      console.log('Correct!');
+    } else if (Number(otvet) !== gcdOtvet) {
+      const gameOver = `'${otvet}' is wrong answer ;(. Correct answer was '${gcdOtvet}'.\nLet's try again, ${userName}!`;
+      console.log(gameOver);
+      return;
+    }
+  }
+  console.log(win);
+};
+gcdGame();
